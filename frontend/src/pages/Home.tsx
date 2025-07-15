31d3a6af2bc54ae39f754dad2416522d354ead94
Karthik DR <karthik.dr@gmail.com>
2025-07-15 13:54:25 +0530

    chore: remove unused React imports for Vercel deploy

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import MainLayout from '../layout/MainLayout';
 import ChatBubble from '../components/ChatBubble';
 import ChatInput from '../components/ChatInput';
